93365628af9f314db2d6bfe7fc05ac9fe5ee9d64
Sundar Ranabhat <sundaranabhat@outlook.com>
2018-05-02 14:32:19 -0500

    WebAPI Demo

---
+++ WebAPIdemo/Controllers/CarDetailsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIdemo.Models;
+
+namespace WebAPIdemo.Controllers
+{
+    public class CarDetailsController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<CarsStock> GetAllcarDetails()
+        {
+            CarsStock ST = new CarsStock();
+            CarsStock ST1 = new CarsStock();
+            List<CarsStock> li = new List<CarsStock>();
+
+            ST.CarName = "Mercedes";
+            ST.CarPrice = 40000;
+            ST.CarModel = "AMD";
+            ST.CarColor = "BLACK";
+
+            ST1.CarName = "BMW ";
+            ST1.CarPrice = 50000;
+            ST1.CarModel = "M3";
+            ST1.CarColor = "RED";
+
+            li.Add(ST);
+            li.Add(ST1);
+            return li;
+        }
+
+        public IEnumerable<CarsStock> Get(int id)
+        {
+            CarsStock ST = new CarsStock();
+            CarsStock ST1 = new CarsStock();
+            List<CarsStock> li = new List<CarsStock>();
+            if (id == 1)
+            {
+
+                ST.CarName = "Mercedes";
+                ST.CarPrice = 40000;
+                ST.CarModel = "AMD";
+                ST.CarColor = "BLACK";
+                li.Add(ST);
+            }
+            else
+            {
+                ST1.CarName = "BMW ";
+                ST1.CarPrice = 50000;
+                ST1.CarModel = "M3";
+                ST1.CarColor = "RED";
+                li.Add(ST1);
+            }
+            return li;
+        }
+
+        [HttpPost]
+        public void PostCar ( int id, [FromBody] CarsStock cs)
+        {
+
+        }
+
+        [HttpPut]
+        public void PutCar ([FromBody]CarsStock cs)
+        {
+
+        }
+
+        [HttpDelete]
+        public void DeleteCar (int id)
+        {
+
+        }
+    }
+}
---
+++ WebAPIdemo/Models/CarsStock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIdemo.Models
+{
+    public class CarsStock
+    {
+        public string CarName { get; set; }
+        public string CarModel { get; set; }
+        public int CarPrice { get; set; }
+        public string CarColor { get; set; }
+
+    }
+}
